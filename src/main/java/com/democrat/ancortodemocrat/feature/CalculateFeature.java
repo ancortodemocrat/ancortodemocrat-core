a94627d2d26ea2f64b61b48294880326912488e5
Slayug <bug.graph@gmail.com>
2016-05-31 10:00:58 +0200

    change calculateToken OK

--- src/main/java/com/democrat/ancortodemocrat/feature/CalculateFeature.java
+++ src/main/java/com/democrat/ancortodemocrat/feature/CalculateFeature.java
@@ -9,22 +9,23 @@ import com.democrat.ancortodemocrat.ConversionWorker;
 import com.democrat.ancortodemocrat.Corpus;
 import com.democrat.ancortodemocrat.Text;
 import com.democrat.ancortodemocrat.element.Annotation;
+import com.democrat.ancortodemocrat.element.Element;
 import com.democrat.ancortodemocrat.element.Relation;
 import com.democrat.ancortodemocrat.element.Unit;
 
 public class CalculateFeature implements Runnable {
-	
+
 	private static Logger logger = Logger.getLogger(CalculateFeature.class);
-	
+
 	private Corpus corpus;
 
 	public CalculateFeature( Corpus corpus ){
 		this.corpus = corpus;
 	}
-	
-	
+
+
 	private void work(){
-		
+
 
 		logger.info("Start converting: [" + corpus.getName() +"]");
 
@@ -40,79 +41,55 @@ public class CalculateFeature implements Runnable {
 
 		logger.info("[" + corpus.getName() +"] done !");
 		corpus.setDone( true );
-		
-		
+
+
 	}
 
 	private void calculateNewFeature( Annotation annotation ) {
-		
-		List<Unit> unitList = annotation.getUnit();
-		
-		for(int u = 0; u < unitList.size(); u++){
 
-			calculatePreviousToken( annotation, unitList.get( u ) );
-			calculateNextToken( annotation, unitList.get( u ) );
-			calculateSpeaker( annotation, unitList.get( u ) );
+		List<Relation> relationList = annotation.getRelation();
+
+		for(int r = 0; r < relationList.size(); r++){
+
+			calculatePreviousNextToken( annotation, relationList.get( r ) );
+			calculateSpeaker( annotation, relationList.get( r ) );
 		}
 	}
-	
-	
-	private void calculatePreviousToken( Annotation annotation, Unit unit ){
-		List<Relation> relationList = annotation.getRelationContaining( unit );
+
+
+	private void calculatePreviousNextToken( Annotation annotation, Relation relation ){
 		Text text = this.corpus.getText( annotation.getFileName() );
 		
-		if( relationList.size() == 1 ){
-			if( relationList.get( 0 ).getElement( annotation ).equals( unit ) ){
-				//the previous is the pre element of the relation
-				unit.setFeature("previous", text.getContentFromUnit( annotation, (Unit) relationList.get( 0 ).getPreElement( annotation ) ) );
-			}else{
-				//to find the previous, we should take the pre-relation
-				//and take the element
-				//or we are in first unit of the coreference, so we put $
-				
-				if( unit.isNew( annotation ) ){
-					//first unit
-					unit.setFeature("previous", "^");
-				}else{
-					unit.setFeature("previous", text.getContentFromUnit( annotation, (Unit) relationList.get( 0 ).getPreRelation( annotation ).getElement( annotation ) ) );
-				}
-			}
-		}
-		//TODO else, unit has many relation, (association, or we are in first mention )
+		Element element = relation.getElement( annotation );
+		Element preElement = relation.getPreElement( annotation );
 		
-	}
-	
-	private void calculateNextToken( Annotation annotation, Unit unit ){
-		List<Relation> relationList = annotation.getRelationContaining( unit );
-		Text text = this.corpus.getText( annotation.getFileName() );
-		
-		if( relationList.size() == 1 ){
-			if( relationList.get( 0 ).getElement( annotation ).equals( unit ) ){
-				//the next is the pre element of the relation
-				//unit.setFeature("previous", text.getContentFromUnit( annotation, (Unit) relationList.get( 0 ).getPreElement( annotation ) ) );
-				//recalculate the relation
-			}else{
-				//to find the next unit, we should take the element of the relation
-				unit.setFeature("previous", text.getContentFromUnit( annotation, (Unit) relationList.get( 0 ).getElement( annotation ) ) );
-			}
-		}else{
-			//range every relation (remove the associatives relations)
+		if( element instanceof Unit && preElement instanceof Unit ){
+			element.setFeature( "previous", text.getContentFromUnit( annotation , (Unit) preElement ) );
+			preElement.setFeature("next", text.getContentFromUnit( annotation , (Unit) element ) );
 			
+			if( ((Unit) preElement).isNew( annotation ) ){
+				//case where the unit hasn't a previous element
+				preElement.setFeature("previous", "^");
+			}
+			//if the unit has only one relation, so he's the last
+			//or a associative mention
+			if( annotation.getRelationContaining( (Unit) element ).size() == 1 ){
+				element.setFeature("next", "$");
+			}
 		}
-		//TODO else, unit has many relation, (association, or we are in first mention )
-		
-	}
-	
 
-	private void calculateSpeaker( Annotation annotation, Unit unit ){
-		
+	}
+
+
+	private void calculateSpeaker( Annotation annotation, Relation unit ){
+
 	}
 
 	@Override
 	public void run() {
 		this.work();
 		this.corpus.export();
-		
+
 	}
 
 }
