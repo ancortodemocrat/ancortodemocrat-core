37bd4f07c0041175a4e88abb9134094470f6656c
Augustin Voisin-Marras <augustinvoima@gmail.com>
2018-05-25 16:11:55 +0200

    Utilisation de commons-args dans Classification + dev

--- src/main/java/com/democrat/classification/Classification.java
+++ src/main/java/com/democrat/classification/Classification.java
@@ -1,14 +1,20 @@
 package com.democrat.classification;
 
+import org.apache.commons.cli.*;
 import org.apache.log4j.Logger;
 import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 import weka.core.converters.ArffSaver;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Vector;
 
 public class Classification {
@@ -16,44 +22,160 @@ public class Classification {
 
     /**
      * Appeler depuis la command model <b>classify</b>
-     * @param commandArgs
+     * @param args
      */
-    public Classification(String[] commandArgs) {
-        //first arg equals to model
-        commandArgs[0] = "";
+    public Classification(String[] args) {
         try {
-            if (commandArgs.length < 3) {
+            ClassifArgs cargs = new ClassifArgs(args);
+            if (args.length < 3) {
                 throw new NoSuchFieldException("ancor2 <model_file> <arff_file>");
             }
-            String model_file = commandArgs[1];
-            String arff_file = commandArgs[2];
+
             ArffLoader loader = new ArffLoader();
 
-            loader.setSource(new FileInputStream(new File(arff_file)));
+            loader.setSource(new FileInputStream(new File(cargs.in_arff)));
             Instances instances = loader.getDataSet();
+
+
+            //Remove remove;
+            /*if( removeAttribute.size() > 0 ){
+
+
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader( new FileReader( arff ) );
+                } catch (FileNotFoundException e2) {
+                    // TODO Auto-generated catch block
+                    e2.printStackTrace();
+                }
+                String line = "";
+
+                int[] indices = new int[ 30 - removeAttribute.size() ];
+                int i = 0;
+                int index = 0;
+                // on crée la liste des indices à utiliser en enlevant ceux de l'utilisateur
+                try {
+                    while( ( line = reader.readLine() ) != null ){
+                        if(line.toLowerCase().contains( "@data" ) ){
+                            break;
+                        }
+                        String attributeName = line.split( " " )[ 1 ];
+                        if( line.contains( "@ATTRIBUTE" ) && ! line.contains( "class" ) ){
+                            if( removeAttribute.contains( attributeName ) ){
+                                indices[ index++ ]  = i;
+                            }
+                            i++;
+                        }
+                    }
+                    System.out.println(indices.toString());
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }finally{
+                    if( reader != null ){
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                remove = new Remove();
+
+                remove.setAttributeIndicesArray( indices );
+                remove.setInvertSelection( false );
+                try {
+                    remove.setInputFormat( instances );
+                    instances = Filter.useFilter( instances, remove );
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }*/
+            //TODO
+
+
             instances.insertAttributeAt(new Attribute("P(CLASS)"),
                     instances.numAttributes()-1);
 
             logger.info("Loaded "+instances.numInstances()+" instances");
 
-            Model model = Model.loadModel(model_file);
+            Model model = Model.loadModel(cargs.model);
 
             logger.info("Classification");
             model.classifyInstanceProba(instances);
-            String dst = arff_file.replace(".arff","_classified.arff");
-            logger.info("Saving results to " + dst);
+
+
+            // Vérifie que le fichier existe
+            if(new File(cargs.out_arff).exists() && ! cargs.force)
+                throw new FileAlreadyExistsException(cargs.out_arff + "existe déjà: utiliser -f ou --force pour écraser ce fichier");
+
+            logger.info("Saving results to " + cargs.out_arff);
             ArffSaver saver = new ArffSaver();
             saver.setInstances(instances);
             try {
-                saver.setFile(new File(dst));
+                saver.setFile(new File(cargs.out_arff));
                 saver.writeBatch();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+            System.err.println(e.getMessage());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class ClassifArgs {
+        private final String in_arff;
+        private final String out_arff;
+        private final String model;
+        private final boolean force;
+
+        public ClassifArgs(String[] args) {
+            Options opt = new Options();
+
+            Option ina = new Option(
+                    "i",
+                    "in-arff",
+                    true,
+                    "Input gold .arff file");
+            ina.setRequired(true);
+            opt.addOption(ina);
+            Option outd= new Option(
+                    "o",
+                    "out-arff",
+                    true,
+                    "Output file for the system .arff");
+            outd.setRequired(true);
+            opt.addOption(outd);
+            Option mod = new Option(
+                    "m",
+                    "model",
+                    true,
+                    "Model to use to classify");
+            mod.setRequired(true);
+            opt.addOption(mod);
+            opt.addOption(
+                    "f",
+                    "force",
+                    false,
+                    "Force overwrite existing system .arff file"
+            );
+
+            CommandLineParser commandline = new GnuParser();
+            CommandLine cmd = null;
+            try {
+                cmd = commandline.parse(opt, args);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+            in_arff = cmd.getOptionValue("i");
+            out_arff = cmd.getOptionValue("o");
+            model = cmd.getOptionValue("m");
+            force = cmd.hasOption("f");
         }
     }
 }
